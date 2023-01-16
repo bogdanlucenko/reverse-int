8c2ee4a6b01de9e77185c35c2944b3a192e43b57
Bohdan Lutsenko <bogdanlucenko@gmail.com>
2023-01-16 17:49:26 +0100

    feat: add function solving task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
-}
+    let x = '';
+    let y = n.toString();
+    let t = y.length - 1;
+    for(let i = 0; i < y.length; i++ ){
+      
+      if(y[i] === '-'){
+      	x = `${''}${x}`;
+      	
+      } else if (y[i] !== '-') {
+      		x = `${y[i]}${x}`;
+      }
+      t = t - 1;
+    }
+    return Number(x);
+  }
